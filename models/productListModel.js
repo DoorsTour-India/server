7f556b3ec6b072d826596dbf213fa2358aa82cc8
kanishk <kanishk.9871@gmail.com>
2021-07-26 18:12:38 +0530

    Added images in ProductLIst

--- models/productListModel.js
+++ models/productListModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const productListSchema = new mongoose.Schema({
   name: String,
   claimPoints: Number,
+  image: String,
 });
 
 const ProductList = mongoose.model('ProductList', productListSchema);
